45af2901a45c14cc591752b5de3ab4b41d9dfe83
mariaDulesova <maria.dulesova@gmail.com>
2021-05-28 15:57:48 +0200

    Title Component

---
+++ src/components/Title.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div class='container'>
+        <img src="@/assets/logo.png">
+        <h2>La mia prima app con Vue CLI!</h2>
+    </div>
+        
+    
+</template>
+
+<script>
+export default {
+    name: 'Title'
+    
+}
+</script>
+
+<style scoped>
+h2 {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+}
+.container {
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+
+}
+
+
+</style>
